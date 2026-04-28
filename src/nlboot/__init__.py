9d831923f4090c6bc98378b4968a18a4fb63bb9b
mdheller <21163552+mdheller@users.noreply.github.com>
2026-04-28 13:11:37 -0400

    Export boot menu planning types

--- src/nlboot/__init__.py
+++ src/nlboot/__init__.py
@@ -1,8 +1,18 @@
 """nlboot protocol reference implementation."""
 
-from .protocol import BootPlan, EnrollmentToken, NlbootError, SignedBootManifest, build_boot_plan
+from .protocol import (
+    BootMenu,
+    BootMenuEntry,
+    BootPlan,
+    EnrollmentToken,
+    NlbootError,
+    SignedBootManifest,
+    build_boot_plan,
+)
 
 __all__ = [
+    "BootMenu",
+    "BootMenuEntry",
     "BootPlan",
     "EnrollmentToken",
     "NlbootError",
